f7b585ce22316c4ef944b34191390317c4d81137
Naqhi <naqhi2001@gmail.com>
2024-01-10 16:34:17 +0530

    third

--- rps.js
+++ rps.js
@@ -1,25 +1,55 @@
-function myChoice(urchoice){
-    console.log(urchoice);
-    const result= document.getElementById('result')
-    
-    const choice= ['Stone','Scissor','Paper'];
-    const cmpchoice = choice[Math.floor(Math.random()*3)]
-    console.log(cmpchoice)
-  if(urchoice ===cmpchoice){
-    result.textContent=('tie');
-    result.style.backgroundColor = 'yellow'
-  }else if((urchoice=='Stone' && cmpchoice=='Scissor')||
-        (urchoice=='Paper' && cmpchoice=='Stone')||
-        (urchoice=='Scissor' && cmpchoice=='paper'))
+let yourscore = 0;
+let opposcore = 0;
 
-    {
-     result.textContent=('you won'); 
-     result.style.backgroundColor = 'lightgreen'
+let maxscore = 5;
+
+let evt = document.querySelectorAll('.choice');
+function myChoice(urchoice) {
+    console.log(urchoice);
+    const result = document.getElementById('result')
+
+    const choice = ['Stone', 'Scissor', 'Paper'];
+    const cmpchoice = choice[Math.floor(Math.random() * 3)]
+    console.log(cmpchoice)
+    if (urchoice === cmpchoice) {
+        result.textContent = ('tie');
+
+        result.style.backgroundColor = 'yellow'
+    } else if ((urchoice == 'Stone' && cmpchoice == 'Scissor') ||
+        (urchoice == 'Paper' && cmpchoice == 'Stone') ||
+        (urchoice == 'Scissor' && cmpchoice == 'paper')) {
+        yourscore++;
     }
-    else{
-        result.textContent=('computer win');
-        result.style.backgroundColor = 'red'
+    else {
+        opposcore++;
     }
-    
-    
+
+    if (yourscore === maxscore) {
+        result.textContent = 'you won';
+        result.style.backgroundColor = 'lightgreen';
+        for(let i of evt){
+            i.style.pointerEvents = 'none'
+        }
+    } else if (opposcore === maxscore) {
+        result.textContent = 'computer win';
+        result.style.backgroundColor = 'red';
+
+        for(let i of evt){
+            i.style.pointerEvents = 'none'
+        }
+    }
+    document.getElementById("your-score").innerText = yourscore;
+    document.getElementById("opponent-score").innerText = opposcore;
+
+} 
+function reset(){
+ yourscore = 0;
+ opposcore = 0;
+ result.textContent = '';
+        result.style.backgroundColor = 'white';
+        document.getElementById("your-score").innerText = 0;
+        document.getElementById("opponent-score").innerText = 0;
+        for(let i of evt){
+            i.style.pointerEvents = 'all'
+        }
 }
